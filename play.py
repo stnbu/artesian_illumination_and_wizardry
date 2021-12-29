11bbd2b202afceb5ec1ed8694487ea45d5cf6a5e
Mike Burr <mburr@unintuitive.org>
2021-12-29 13:23:59 -0700

    .

--- play.py
+++ play.py
@@ -5,6 +5,13 @@ from PyQt5.QtWidgets import QApplication, QWidget
 
 points = [(0, 90), (100, 140), (220, 310), (420, 370), (510, 330)]
 
+circle = b"""
+<svg height="100" width="100">
+  <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+  Sorry, your browser does not support inline SVG.
+</svg>
+"""
+
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
@@ -12,12 +19,12 @@ class MainWindow(QWidget):
         self.widgetSvg = QSvgWidget(parent=self)
         self.widgetSvg.setGeometry(10, 10, 1080, 1080)
 
-        self.frame = svgwrite.Drawing("/tmp/aiw.svg", profile="tiny")
-        self.frame.add(self.frame.polygon(points, fill="blue"))
+        # self.frame = svgwrite.Drawing("/tmp/aiw.svg", profile="tiny")
+        # self.frame.add(self.frame.polygon(points, fill="blue"))
+        # xml = self.frame.tostring()
+        # self.xml = QByteArray(bytes(xml, encoding='utf8'))
 
-        xml = self.frame.tostring()
-        self.xml = QByteArray(bytes(xml, encoding='utf8'))
-        self.widgetSvg.load(self.xml)
+        self.widgetSvg.load(circle)
 
 if __name__ == "__main__":
     app = QApplication([])
