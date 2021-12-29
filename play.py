96594efd37b07bf64799f2589f0b692609ed6363
Mike Burr <mburr@unintuitive.org>
2021-12-29 13:37:29 -0700

    .

--- play.py
+++ play.py
@@ -1,22 +1,9 @@
-import svgwrite, webbrowser, math, os
+import svgwrite
 from PyQt5.QtCore import QByteArray
 from PyQt5.QtSvg import QSvgWidget
 from PyQt5.QtWidgets import QApplication, QWidget
 
 points = [(0, 90), (10, 14), (220, 31), (420, 37), (510, 33)]
-
-circle = b"""
-<svg height="100" width="100">
-  <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
-  Sorry, your browser does not support inline SVG.
-</svg>
-"""
-
-my_blob = b"""
-<svg height="100%" width="100%">
-<polygon fill="blue" points="0,90 10,14 22,31 42,37 51,33" />
-</svg>
-"""
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -24,12 +11,9 @@ class MainWindow(QWidget):
         self.setGeometry(100, 100, 1100, 1100)
         self.widgetSvg = QSvgWidget(parent=self)
         self.widgetSvg.setGeometry(10, 10, 1080, 1080)
-
-        frame = svgwrite.Drawing("/tmp/aiw.svg", profile="tiny")
+        frame = svgwrite.Drawing(None, profile="tiny")
         frame.add(frame.polygon(points, fill="blue"))
-        xxx = bytes(frame.tostring(), encoding='utf8')
-
-        self.widgetSvg.load(xxx)
+        self.widgetSvg.load(bytes(frame.tostring(), encoding='utf8'))
 
 if __name__ == "__main__":
     app = QApplication([])
