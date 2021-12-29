a3e59a31a5a0121e3516595e1037a5bf5e18b93b
Mike Burr <mburr@unintuitive.org>
2021-12-29 13:15:55 -0700

    .

--- play.py
+++ play.py
@@ -1,65 +1,80 @@
-import svgwrite, webbrowser, math
+import svgwrite, webbrowser, math, os
 from sympy import Matrix
+from PyQt5.QtCore import QByteArray
+from PyQt5.QtSvg import QSvgWidget
+from PyQt5.QtWidgets import QApplication, QWidget
 
-f = "/tmp/thing.svg"
+def get_rotate_matrix(angle):
+    return Matrix(
+        [
+            [math.cos(angle), -math.sin(angle)],
+            [math.sin(angle), math.cos(angle)],
+        ]
+    )
 
-import svgwrite 
+points = [(0, 90), (100, 140), (220, 310), (420, 370), (510, 330)]
 
-path = [(100,100),(100,200),(200,200),(200,100)]
+num_frames = 90
+total_angle = math.pi / 4
+angle_delta = num_frames / num_frames
+delta_rotate_matrix = get_rotate_matrix(angle_delta)
 
-image = svgwrite.Drawing('test.svg',size=(300,300))
+#os.makedirs("build", exist_ok=True)
 
-rectangle = image.add(image.polygon(path,id ='polygon',stroke="black",fill="white"))
-rectangle.add(image.animateTransform("rotate","transform",id="polygon", from_="0 150 150", to="360 150 150",dur="4s",begin="0s",repeatCount="indefinite"))
-text = image.add(image.text('rectangle1',insert=(150,30),id="text"))
-text.add(image.animateColor("fill", attributeType="XML",from_="green", to="red",id="text", dur="4s",repeatCount="indefinite"))
+for index in range(0, num_frames):
+    continue  # pass
+    frame = svgwrite.Drawing("build/xframe_%04d.svg" % index, profile="tiny")
+    frame.add(frame.polygon(points, fill="blue"))
+    frame.save()
+    for jndex, point in enumerate(points):
+        point = delta_rotate_matrix * Matrix(point)
+        points[jndex] = [int(c) for c in point]
 
-image.save()
+from xml.etree import ElementTree
 
-import sys; sys.exit()
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(100, 100, 1100, 1100)
+        self.widgetSvg = QSvgWidget(parent=self)
+        self.widgetSvg.setGeometry(10, 10, 1080, 1080)
+        self.frame = svgwrite.Drawing("/tmp/xyzzz2.svg")
+        xml = self.frame.tostring()
+        xml = QByteArray(bytes(xml, encoding='utf8'))
+        self.widgetSvg.load(xml)
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec()
 
 
-
-
-
-
-
-drawing = svgwrite.Drawing(f, profile='tiny')
-
-theta = math.pi / 2
-
-
-def rotate(points, angle):
-    result = []
-    rotato = Matrix([
-        [math.cos(angle), - math.sin(angle)],
-        [math.sin(angle),   math.cos(angle)],
-    ])
-    for pair in points:
-        thing = rotato * Matrix(pair)
-        result.append((int(thing[0]), int(thing[1])))
-    #print(result)
-    return result
-
-# drawing.add(
-#     drawing.rect(
-#         (10, 10),
-#         (300, 200),
-#         stroke=svgwrite.rgb(10, 10, 16, '%'),
-#         fill='red'
+##################
+# path = [(100, 100), (100, 200), (200, 200), (200, 100)]
+# image = svgwrite.Drawing("test.svg", size=(300, 300))
+# rectangle = image.add(image.polygon(path, id="polygon", stroke="black", fill="white"))
+# rectangle.add(
+#     image.animateTransform(
+#         "rotate",
+#         "transform",
+#         id="polygon",
+#         from_="0 150 150",
+#         to="360 150 150",
+#         dur="4s",
+#         begin="0s",
+#         repeatCount="indefinite",
 #     )
 # )
-
-randoid_points = [(0, 90), (100, 140), (220, 310), (420, 370), (510, 330)]
-    
-drawing.add(
-    drawing.polygon(randoid_points, fill='blue')
-)
-
-drawing.add(
-    drawing.polygon(rotate(randoid_points, math.pi / 5), fill='green')
-)
-
-drawing.save()
-
-#webbrowser.open('file://' + f, new=0, autoraise=True)
+# text = image.add(image.text("rectangle1", insert=(150, 30), id="text"))
+# text.add(
+#     image.animateColor(
+#         "fill",
+#         attributeType="XML",
+#         from_="green",
+#         to="red",
+#         id="text",
+#         dur="4s",
+#         repeatCount="indefinite",
+#     )
+# )
