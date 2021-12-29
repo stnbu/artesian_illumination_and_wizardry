0b1a289bbb19673871e7b21cd721b3858d838530
Mike Burr <mburr@unintuitive.org>
2021-12-29 14:23:46 -0700

    but maybe no file_to_bytes...

--- play.py
+++ play.py
@@ -7,28 +7,25 @@ size = 30
 
 points = [(0, 0), (size, 0), (size, size), (0, size)]
 
+SVG_PATH = "/tmp/aiw.svg"
+
+def path_to_bytes(path):
+    return open(path, "rb").read()
+
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
         self.file_watcher = QFileSystemWatcher()
         self.file_watcher.fileChanged.connect(self.on_file_changed)
-        self.file_watcher.addPaths(["/tmp/aiw.svg"])
+        self.file_watcher.addPaths([SVG_PATH])
 
         self.palette = QSvgWidget(parent=self)
         self.palette.setGeometry(10, 10, 1080, 1080)
-
-        frame = svgwrite.Drawing(None)
-        frame.add(frame.polygon(points, fill="blue"))
-
-        self.palette.load(bytes(frame.tostring(), encoding='utf8'))
+        self.palette.load(path_to_bytes(SVG_PATH))
 
     def on_file_changed(self, path):
-        print(("---------------------------\n"
-              "I changed!!! -->\n"
-              "  str(%s)\n"
-              "  type(%s)\n"
-              "  repr(%s)\n\n") % (path, type(path), repr(path)))
+        self.palette.load(path_to_bytes(path))
 
 if __name__ == "__main__":
     app = QApplication([])
