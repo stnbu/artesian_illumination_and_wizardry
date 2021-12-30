2ba92076eaa0e34806159cb55721cb487fe19e74
Mike Burr <mburr@unintuitive.org>
2021-12-30 07:55:55 -0700

    .

--- maybelib.py
+++ maybelib.py
@@ -1,25 +1,30 @@
 import svgwrite, webbrowser, math, os
 from sympy import Matrix
 
+ROTATE = 0
+SCALE = 1
 
-def transform(points, **transforms):
-    rotate = transforms.pop("rotate")
+def transform(points, transforms):
+    for transform in transforms:
+        if transform['type'] == ROTATE:
+            angle = transform["angle"]
+            about_x, about_y = transform["about"]
+            r = Matrix(
+                [
+                    [math.cos(angle), -math.sin(angle)],
+                    [math.sin(angle), math.cos(angle)],
+                ]
+            )
+            rotated_about_x, rotated_about_y = r * Matrix([about_x, about_y])
+            d_x, d_y = rotated_about_x - about_x, rotated_about_y - about_y
+            for point in points:
+                x, y = r * Matrix(point)
+                yield int(x - d_x), int(y - d_y)
+        elif transform['type'] == SCALE:
+            raise NotImplementedError
+        else:
+            raise ValueError("Unknown transform type %s" % transform['type'])
 
-    angle = rotate["angle"]
-    about_x, about_y = rotate["about"]
-
-    r = Matrix(
-        [
-            [math.cos(angle), -math.sin(angle)],
-            [math.sin(angle), math.cos(angle)],
-        ]
-    )
-
-    rotated_about_x, rotated_about_y = r * Matrix([about_x, about_y])
-    d_x, d_y = rotated_about_x - about_x, rotated_about_y - about_y
-    for point in points:
-        x, y = r * Matrix(point)
-        yield int(x - d_x), int(y - d_y)
 
 
 if __name__ == "__main__":
@@ -27,7 +32,15 @@ if __name__ == "__main__":
     points = [(0, 0), (50, 0), (50, 50), (0, 50)]
     for i in range(0, 50):
         frame = svgwrite.Drawing("foo.svg", profile="tiny")
-        p = transform(points, rotate=dict(angle=(math.pi / 1000)*i*2, about=[50, 50]))
+        p = transform(
+            points,
+            [
+                dict(
+                    type=ROTATE,
+                    angle=(math.pi / 1000)*i*2,
+                    about=[50, 50])
+            ]
+        )
         frame.add(frame.polygon(p, fill="blue"))
         frame.add(frame.circle(center=(25, 25), r=1, fill="black"))
         frame.save()
