e654067c880a7ad1eef51d1635ff97072c1f569e
Mike Burr <mburr@unintuitive.org>
2022-01-02 10:22:50 -0700

    .

--- graphing.py
+++ graphing.py
@@ -1,6 +1,7 @@
 import math, os, svgwrite
 
-NEW_ORIGIN = 100, 100
+scale = 300
+NEW_ORIGIN = scale, scale
 
 def C(x, y):
     dx, dy = NEW_ORIGIN
@@ -9,11 +10,11 @@ def C(x, y):
 frame = svgwrite.Drawing("foo2.svg", profile="tiny")
 
 for i in range(0, 20):
-    thing = 100 - (10 * i)
-    frame.add(frame.line(start=C(-100, thing), end=C(100, thing), stroke='lightgrey', stroke_width=1))
-    frame.add(frame.line(start=C(thing, 100), end=C(thing, -100), stroke='lightgrey', stroke_width=1))
+    t = scale - ((scale/10) * i)
+    frame.add(frame.line(start=C(-scale, t), end=C(scale, t), stroke='lightgrey', stroke_width=1))
+    frame.add(frame.line(start=C(t, scale), end=C(t, -scale), stroke='lightgrey', stroke_width=1))
 
-frame.add(frame.line(start=C(-100, 0), end=C(100, 0), stroke='red', stroke_width=1))
-frame.add(frame.line(start=C(0, 100), end=C(0, -100), stroke='red', stroke_width=1))
+frame.add(frame.line(start=C(-scale, 0), end=C(scale, 0), stroke='red', stroke_width=1))
+frame.add(frame.line(start=C(0, scale), end=C(0, -scale), stroke='red', stroke_width=1))
 
 frame.save()
