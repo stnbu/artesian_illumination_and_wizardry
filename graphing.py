17dcf7883679638df313731fd4c1b49e205737b7
Mike Burr <mburr@unintuitive.org>
2022-01-02 11:02:27 -0700

    .

--- graphing.py
+++ graphing.py
@@ -9,19 +9,24 @@ def C(x, y):
 
 frame = svgwrite.Drawing("foo2.svg")
 
-frame.defs.add(frame.style('* {font-size: 12px;}'))
+frame.defs.add(frame.style('* {font-size: 9px;}'))
 
 for i in range(0, 20):
     t = scale - ((scale/10) * i)
+    if t == 0:
+        continue
+
+    text_x, text_y = C(3, -11)
     frame.add(frame.line(start=C(-scale, t), end=C(scale, t), stroke='lightgrey', stroke_width=1))
     frame.add(frame.circle(C(t, 0), 1, fill='black', stroke='black'))
+    frame.add(frame.text("%d" % (t//50), x=[text_x + t], y=[text_y], fill='black'))
+
     frame.add(frame.line(start=C(t, scale), end=C(t, -scale), stroke='lightgrey', stroke_width=1))
     frame.add(frame.circle(C(0, t), 1, fill='black', stroke='black'))
+    frame.add(frame.text("%d" % (t//50), x=[text_x], y=[text_y + t], fill='black'))
 
 frame.add(frame.line(start=C(-scale, 0), end=C(scale, 0), stroke='red', stroke_width=1))
 frame.add(frame.line(start=C(0, scale), end=C(0, -scale), stroke='red', stroke_width=1))
 frame.add(frame.circle(C(0, 0), 1, fill='black', stroke='black'))
-x, y = C(3, -12)
-frame.add(frame.text("0,0", x=[x], y=[y], fill='black'))
 
 frame.save()
