3d84a411f40f7793122ca8a96a73ed2e126f349e
Mike Burr <mburr@unintuitive.org>
2022-01-02 10:18:14 -0700

    .

--- graphing.py
+++ graphing.py
@@ -9,8 +9,9 @@ def C(x, y):
 frame = svgwrite.Drawing("foo2.svg", profile="tiny")
 
 for i in range(0, 20):
-    frame.add(frame.line(start=C(-100, 100-(10*i)), end=C(100, 100 - (10*i)), stroke='lightgrey', stroke_width=1)) # I am horizontal
-    frame.add(frame.line(start=C(100-(10*i), 100), end=C(100-(10*i), -100), stroke='lightgrey', stroke_width=1))
+    thing = 100 - (10 * i)
+    frame.add(frame.line(start=C(-100, thing), end=C(100, thing), stroke='lightgrey', stroke_width=1))
+    frame.add(frame.line(start=C(thing, 100), end=C(thing, -100), stroke='lightgrey', stroke_width=1))
 
 frame.add(frame.line(start=C(-100, 0), end=C(100, 0), stroke='red', stroke_width=1))
 frame.add(frame.line(start=C(0, 100), end=C(0, -100), stroke='red', stroke_width=1))
